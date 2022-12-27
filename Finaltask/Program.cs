8848281c6c15308e8e5e9cd9d3f94438bb5b568f
Granat54 <ladukun.t.red@mail.ru>
2022-12-27 13:08:16 +0300

    SortArray

--- Finaltask/Program.cs
+++ Finaltask/Program.cs
@@ -1,12 +1,12 @@
-﻿string[] CreateArrayRndInt (int size)
+﻿string[] CreateArrayRndInt(int size)
 {
     string[] array = new string[size];
-    
+
     for (int i = 0; i < array.Length; i++)
     {
         Console.WriteLine("Введите значение:");
         array[i] = Console.ReadLine();
-    }        
+    }
     return array;
 }
 
@@ -15,23 +15,38 @@ void PrintArray(string[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.Write($"{array[i]}");
     }
     Console.Write("]");
 }
 
 
-int CountLessFour(string[] arr)
+string[] SortLessFour(string[] arr)
 {
     int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length <=3) count++;
+        if (arr[i].Length <= 3) count++;
     }
-    return count;
+
+    string[] newArray = new string[count];
+    int k = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArray[k] = arr[i];
+            k++;
+        }
+    }
+    return newArray;
 }
+
 
 string[] array = CreateArrayRndInt(4);
 PrintArray(array);
-int newSize = CountLessFour(array);
+string[] newArray = SortLessFour(array);
+Console.Write(" -> ");
+PrintArray(newArray);
