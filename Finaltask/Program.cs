2b06b1b4aa7909a2a4d9754c9cb669cdec9468d4
Granat54 <ladukun.t.red@mail.ru>
2022-12-27 12:50:40 +0300

    createArray

--- Finaltask/Program.cs
+++ Finaltask/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArrayRndInt (int size)
+{
+    string[] array = new string[size];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите значение:");
+        array[i] = Console.ReadLine();
+    }        
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+string[] array = CreateArrayRndInt(4);
+PrintArray(array);
